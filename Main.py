44f55242f689eefb64fc53cf07fd68f3b1ef297f
kenny <64386216+keennneeth@users.noreply.github.com>
2022-11-07 15:38:53 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,18 +11,18 @@ for i in range(0, len(randomlist), chunk_size):
 newlist = chunked_list
 
 print ("(a) create a 5 by 6 matrix, A, whose entries are random integers between 0 and 10.")
+print("_"*75)
+print("\nMATRIX 1")
+print("_"*75)
+
+
 A = newlist
 print(*A, sep = "\n")
-
-
 
 print("\n(b) Find the transpose of, B, of this matrix")
 B = list(map(list,zip(*chunked_list)))
 for row in B:
     print(row)
-
-
-
 
 result = [[0,0,0,0,0],
           [0,0,0,0,0],
@@ -30,29 +30,104 @@ result = [[0,0,0,0,0],
           [0,0,0,0,0],
           [0,0,0,0,0]]
 
-
 print("\n(c)find the product AB.")
-
 for i in range(len(A)):
- 
-    # iterating by column by B
-    for j in range(len(B[0])):
- 
-        # iterating by rows of B
+     for j in range(len(B[0])):
         for k in range(len(B)):
             result[i][j] += A[i][k] * B[k][j]
- 
 for r in result:
     print(r)
 
 
 
+result1 = [[0,0,0,0,0,0],
+           [0,0,0,0,0,0],
+           [0,0,0,0,0,0],
+           [0,0,0,0,0,0],
+           [0,0,0,0,0,0],
+           [0,0,0,0,0,0]]
+print("\n(d)find the product BA.")
+for i in range(len(B)):
+     for j in range(len(A[0])):
+        for k in range(len(A)):
+            result1[i][j] += B[i][k] * A[k][j]
+for r in result1:
+    print(r)
+
+print("_"*75)
+print("\n(e) repeat the sequence of steps (a) through (d) two more times")
+print("_"*75)
+
+print("\nMATRIX 2")
+print("_"*75)
+print("\n")
+print("(a) Create a 5x6 matrix\n")
+
+randomlist1 = [random.randrange(10) for i in range (30)]
+chunked_list = list()
+chunk_size = 6
+
+for i in range(0, len(randomlist), chunk_size):
+    chunked_list.append(randomlist1[i:i+chunk_size])
+newlist1 = chunked_list
+A1 = newlist1
+print(*A1, sep = "\n")
+
+print("\n(b) Find the transpose of, B, of this matrix")
+B1 = list(map(list,zip(*chunked_list)))
+for row in B1:
+    print(row)
+
+print("\n(c)find the product AB.")
+for i in range(len(A1)):
+     for j in range(len(B1[0])):
+        for k in range(len(B1)):
+            result[i][j] += A1[i][k] * B1[k][j]
+for r in result:
+    print(r)
+
+print("\n(c)find the product BA.")
+for i in range(len(B1)):
+     for j in range(len(A1[0])):
+        for k in range(len(A1)):
+            result1[i][j] += B1[i][k] * A1[k][j]
+for r in result1:
+    print(r)
 
 
-# print("\n(d) find the product BA.")
+#MATRIX 3
+print("_"*75)
+print("\nMATRIX 3")
+print("_"*75)
+print("(a) Create a 5x6 matrix\n")
 
-# for i in range (len(A)):
-#     for j in range (len(B)):
-#         result[i][j] = B[i][j] * A[i][j]
-# for r in result:
-#     print(r)
+randomlist2 = [random.randrange(10) for i in range (30)]
+chunked_list = list()
+chunk_size = 6
+
+for i in range(0, len(randomlist), chunk_size):
+    chunked_list.append(randomlist2[i:i+chunk_size])
+newlist2 = chunked_list
+A2 = newlist2
+print(*A1, sep = "\n")
+
+print("\n(b) Find the transpose of, B, of this matrix")
+B2 = list(map(list,zip(*chunked_list)))
+for row in B2:
+    print(row)
+
+print("\n(c)find the product AB.")
+for i in range(len(A2)):
+     for j in range(len(B2[0])):
+        for k in range(len(B2)):
+            result[i][j] += A2[i][k] * B2[k][j]
+for r in result:
+    print(r)
+
+print("\n(c)find the product BA.")
+for i in range(len(B2)):
+     for j in range(len(A2[0])):
+        for k in range(len(A2)):
+            result1[i][j] += B2[i][k] * A2[k][j]
+for r in result1:
+    print(r)
