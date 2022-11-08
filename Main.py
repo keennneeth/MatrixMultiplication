fd9317673a660996a4099776a45c8d47151d9969
kenny <64386216+keennneeth@users.noreply.github.com>
2022-11-08 17:24:54 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ from itertools import chain
 
 
 randomlist = [random.randrange(10) for i in range (30)]
-chunked_list = list()
+chunked_list = list()                       #Creating a random number from 0-10, 30 times
 chunk_size = 6
 
 for i in range(0, len(randomlist), chunk_size):
-    chunked_list.append(randomlist[i:i+chunk_size])
+    chunked_list.append(randomlist[i:i+chunk_size])          #putting it into groups of 6
 
 newlist = chunked_list
 
@@ -24,11 +24,9 @@ A = newlist
 print(*A, sep = "\n")
 
 print("\n(b) Find the transpose of, B, of this matrix")
-B = list(map(list,zip(*chunked_list)))
+B = list(map(list,zip(*chunked_list)))                  #zip function makes it into the transpose
 for row in B:
     print(row)
-
-
 
 result = [[0,0,0,0,0],
           [0,0,0,0,0],
@@ -38,7 +36,7 @@ result = [[0,0,0,0,0],
 
 print("\n(c)find the product AB.")
 for i in range(len(A)):
-     for j in range(len(B[0])):
+     for j in range(len(B[0])):                 #multiplying A*B
         for k in range(len(B)):
             result[i][j] += A[i][k] * B[k][j]
 for r in result:
@@ -54,7 +52,7 @@ result1 = [[0,0,0,0,0,0],
            [0,0,0,0,0,0]]
 print("\n(d)find the product BA.")
 for i in range(len(B)):
-     for j in range(len(A[0])):
+     for j in range(len(A[0])):             #multiplying B*A
         for k in range(len(A)):
             result1[i][j] += B[i][k] * A[k][j]
 for r in result1:
@@ -149,16 +147,12 @@ for i in range(0, len(negrandomlist), chunk_size):
 
 negnewlist = chunked_list
 
-
 print("\n(g) Create a negative 5x6 matrix")
 
 negA = negnewlist
 print(*negA, sep = "\n")
 print("\n(REFERENCE (BELOW IS MATRIX A)")
 print(*A, sep = "\n")
-
-
-
 
 print("\n(h) Computer A+C")
 
@@ -171,7 +165,7 @@ for r in resultAC:
 print("\n(i) Find the sum of all even numbers in A+C\n")
 print("Even numbers in A + C below")
 print("_"*75)
-allrows = list(chain(resultAC[0], resultAC[1], resultAC[2], resultAC[3], resultAC[4]))
+allrows = list(chain(resultAC[0], resultAC[1], resultAC[2], resultAC[3], resultAC[4]))          #combining the lists by using chain
 
 
 mylist = []
@@ -179,26 +173,22 @@ for num1 in allrows:
     if num1 % 2 == 0:
         mylist.append(num1)
 print(mylist)
-total = sum(mylist)
+total = sum(mylist)                     #making the even numbers into a list
 print("_"*75)
 
 
 print("\nTotal sum (even): ",total)
 print("\n")
 
-
-
-
 print("(j) Computer A-C")
-print("For Reference")
-print("\nBELOW IS MATRIX A")
+print("\nREFERENCE (BELOW IS MATRIX A)")
 print(*A, sep = "\n")
 print("\nBELOW IS MATRIX C")
 print(*negA, sep = "\n")
 print("\nBELOW IS A-C")
 
 subresultAC = [[A[i][j] - negA[i][j]  for j in range(len(A[0]))] for i in range(len(A))]
-for r in subresultAC:
+for r in subresultAC:                                                                       #subtracting A-C
    print(r)
 
 
@@ -209,7 +199,7 @@ print("_"*75)
 
 mylist = []
 for num2 in allrows1:
-    if num2 % 2 == 1:
+    if num2 % 2 == 1:           #putting odd numbers into a list
         mylist.append(num2)
 print(mylist)
 total = sum(mylist)
